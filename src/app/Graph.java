fc5c70f6787b070ffc60ce00074400c69b906120
Bohdan <blazarchuk@gmail.com>
2025-11-29 20:40:37 +0100

    Initial commit

---
+++ src/app/Graph.java
@@ -0,0 +1,28 @@
+package app;
+
+public class Graph {
+
+    void addVertex(int vertex){
+
+    }
+
+    void addEdge(int source, int destination){
+
+    }
+
+   void removeVertex(int vertex){
+
+   }
+
+   void removeEdge(int source, int destination){
+
+   }
+
+   void hasVertex(int vertex){
+
+   }
+
+    void hasEdge(int source, int destination){
+
+    }
+}
